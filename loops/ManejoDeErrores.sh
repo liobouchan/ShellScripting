4bcaa24fb85f62c96ae56c52b709d0c848b63a0d
Leonardo <liogothicvampire@gmail.com>
2019-07-12 15:12:21 -0500

    Antes de hacer un cambio brutal

--- loops/ManejoDeErrores.sh
+++ loops/ManejoDeErrores.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 numberOfArguments=$#
+y=$(($numberOfArguments))
+
+echo " ARGUMENTOS $numberOfArguments"
 
 if [ $numberOfArguments -ne 0 ]; then
   for comando in "$@"; do
     i=$(($i+1))
-    echo $i
-    echo "perreo ${!i}"
-    executed_comando=${comando}
-      echo "Ejecutando $executed_comando"
-      $executed_comando
-
-      exit_comando=$?
-      echo "Salida $exit_comando"
-
-      if [ "$exit_comando" -ne 0 ]; then
-        echo "  Ocurrió un error -- Reintentando $executed_comando"
+    if [ $i -lt $numberOfArguments ]; then
+      
+      #echo $i
+      echo "Este comandito $comando"
+      echo "numero ${!y}"
+      executed_comando=${comando}
+        echo "Ejecutando $executed_comando"
         $executed_comando
 
         exit_comando=$?
-        if [ "$exit_comando" -eq 0 ]; then
-          echo "    El error fué solucionado al reintentar"
-          else
-            echo "    El error sigue persistiendo"
-            echo "    Último comando ejecutado: $executed_comando"
-            echo "    Último código de salida: $exit_comando"
+        echo "Salida $exit_comando"
 
-            echo "    Insertar aquí comando para reestablecer todo"
-            
-            exit 1
+        if [ "$exit_comando" -ne 0 ]; then
+          echo "  Ocurrió un error -- Reintentando $executed_comando"
+          $executed_comando
+
+          exit_comando=$?
+          if [ "$exit_comando" -eq 0 ]; then
+            echo "    El error fué solucionado al reintentar"
+            else
+              echo "    El error sigue persistiendo"
+              echo "    Último comando ejecutado: $executed_comando"
+              echo "    Último código de salida: $exit_comando"
+
+              echo "    Insertar aquí comando para reestablecer todo"
+              
+              exit 1
+          fi
         fi
-      fi
+    fi
   done
   else
     echo "No se han recibido los parámetros"
--- loops/sendArray.sh
+++ loops/sendArray.sh
@@ -3,7 +3,13 @@ declare -a COMANDOS
 COMANDOS[1]="cp 1.txt 2.txt"
 COMANDOS[2]="cat 2.txt"
 COMANDOS[3]="cp 2.txt 3.txt"
-COMANDOS[222]="NOMBRE"
+#COMANDOS[222]="NOMBRE"
+
+declare -a COMANDOS2
+COMANDOS2[1]="cp 1.txt 2.txt"
+COMANDOS2[2]="cat 2.txt"
+COMANDOS2[3]="cp 2.txt 3.txt"
+#COMANDOS2[222]="NOMBRE"
 
 recividor=./ManejoDeErrores.sh
-$recividor "${COMANDOS[@]}"
+$recividor "${COMANDOS[@]}" "${COMANDOS2[@]}"
